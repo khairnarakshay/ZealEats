de6e13b056ae3c3a4e91e4910d618b8ec3e69d2c
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-02-06 15:41:26 +0530

    update desgn

--- CCFOS/canteen/urls.py
+++ CCFOS/canteen/urls.py
@@ -1,6 +1,11 @@
-from  django.urls import path
+# 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mycart/', views.MyCart, name='mycart'),
+    path('myorder/', views.MyOrder, name='myorder'),
+    path('aboutus/', views.AboutUs, name='aboutus'),
+    path('contactus/', views.ContactUs, name='contactus'),
 ]
--- CCFOS/canteen/views.py
+++ CCFOS/canteen/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def MyCart(request):
+    return render(request, 'mycart.html')
+
+def MyOrder(request):
+    return render(request, 'myorder.html')
+
+
+def AboutUs(request):
+    return render(request, 'aboutus.html')
+
+
+def ContactUs(request):
+    return render(request, 'contactus.html')
+
+
+
+
