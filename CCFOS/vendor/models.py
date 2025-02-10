5b2971cd2cc03fba438fdcfd57fd9c792cdce084
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-02-10 17:23:39 +0530

    vendor registration model are created

--- CCFOS/vendor/models.py
+++ CCFOS/vendor/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
+class CanteenVendor(models.Model):
+    full_name = models.CharField(max_length=100)
+    restaurant_name = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=15)
+    store_description = models.TextField()
+    password = models.CharField(max_length=255)  # It is recommended to hash passwords
+    confirm_password = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.restaurant_name
