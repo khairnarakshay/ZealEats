1efbde6baca08064096d9db4c57d0444fadb834e
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-02-22 18:17:38 +0530

    add categories

--- CCFOS/vendor/models.py
+++ CCFOS/vendor/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 
 class CanteenVendor(models.Model):
+    id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=100)
     restaurant_name = models.CharField(max_length=100)
-    mobile_number = models.CharField(max_length=15)
+    mobile_number = models.CharField(max_length=100)
     store_description = models.TextField()
     password = models.CharField(max_length=255)  # It is recommended to hash passwords
-    confirm_password = models.CharField(max_length=255)
+    is_approved = models.BooleanField(default=False)  # Admin approval status
     
     def __str__(self):
         return self.restaurant_name
+
+class FoodItem(models.Model):
+    #reasturant_name = models.ForeignKey(CanteenVendor, on_delete=models.CASCADE, related_name='food_items_restaurant')
+    vendor = models.ForeignKey(CanteenVendor, on_delete=models.CASCADE)# Reference vendor ID
+    food_name = models.CharField(max_length=100)
+    description = models.TextField()
+    TYPE_CHOICES = [('Veg', 'Veg'), ('Non-Veg', 'Non-Veg')]
+    food_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    CATEGORY_CHOICES = [('Breakfast', 'Breakfast'), ('Drinks', 'Drinks'),
+                        ('Lunch', 'Lunch'), ('Snacks', 'Snacks'), ('Dinner', 'Dinner')]
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images/')  # Store in media folder
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.food_name} - {self.vendor.restaurant_name}"
+    
