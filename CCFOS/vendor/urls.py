9783e1f8eacae0b95b362e3f24c8833f9f9b0c0a
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-02-23 01:08:40 +0530

    add a add_items url

--- CCFOS/vendor/urls.py
+++ CCFOS/vendor/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     #path('home/', views.dashboard_home, name='vendor_home'),
-    path('login/', views.login, name='login'),
+    #path('admin/logout/',views.admin_logout, name='admin_logout'),  # Custom admin logout
+    path('login/', views.vendor_login, name='login'),
+    path('logout/', views.vendor_logout, name='logout'),
     path('register/', views.register_view, name='register'),  # New registration URL
     path('dashboard/', views.dashboard, name='dashboard'),
     path('order/', views.order, name='order'),
-    path('items/', views.items, name='items'),
+    path('additems/', views.additems, name='additems'),
+    path('manage-items/', views.manage_items, name='manage_items'),
+    path("update-items/<int:id>",views.update_items, name="update-item"),
+    
+    path("delete-item/<int:item_id>/", views.delete_item, name="delete-item"),
     path('account/', views.account, name='account'),
-]
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
