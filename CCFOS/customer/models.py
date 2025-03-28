7ffe6d4e5bfe70f5f326c651434510f5ca149de1
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-03-28 18:52:00 +0530

    update a cart model add total

--- CCFOS/customer/models.py
+++ CCFOS/customer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from vendor.models import FoodItem
 # Create your models here.
 
 class Customer(models.Model):
@@ -11,3 +11,29 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.full_name
+
+# Cart Model
+
+
+# CartItem Model
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE , null=False,default="")  # Link to Customer
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart for {self.customer.full_name}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2 , default=0.00)
+    
+
+    def __str__(self):
+        return f"{self.food_item.food_name} - {self.quantity}"
+
+    def total_price(self):
+        return self.quantity * self.price
