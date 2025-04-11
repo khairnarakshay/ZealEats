5c0536d696e9e0eb6da02f4012e85063ebec912a
Akshay Khairnar <khairnarakshay1722@gmail.com>
2025-04-11 13:09:15 +0530

    Update About us Page

--- CCFOS/canteen/urls.py
+++ CCFOS/canteen/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mycart/', views.MyCart, name='mycart'),
-    path('myorder/', views.MyOrder, name='myorder'),
+   # path('view_order/', views.view_order, name='view_order'),
     path('aboutus/', views.AboutUs, name='aboutus'),
     path('contactus/', views.ContactUs, name='contactus'),
 ]
--- CCFOS/canteen/views.py
+++ CCFOS/canteen/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from vendor.models import FoodItem , CanteenVendor
+from customer.models import Customer 
 # Create your views here.
+# def home(request):
+#     food_items = FoodItem.objects.all()
+#     customer_name = request.session.get('customer_name','')
+#     context = {'user_logged_in': request.session.get('user_id') is not None}
+#     print(food_items)
+    
+#     # Pass both context variables in a single dictionary
+#     return render(request, 'home.html', {'food_items': food_items, 'customer_name': customer_name, 'context': context})
 def home(request):
     food_items = FoodItem.objects.all()
-    print(food_items)
-    return render(request, 'home.html', {'food_items': food_items})
+    customer_name = request.session.get('customer_name', '')
+    user_logged_in = request.session.get('user_id') is not None
+
+    # Merge all variables in a single context dictionary
+    context = {
+        'food_items': food_items,
+        'customer_name': customer_name,
+        'user_logged_in': user_logged_in
+    }
+
+    return render(request, 'home.html', context)
 
 
 def MyCart(request):
